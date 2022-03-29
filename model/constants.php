0db8e46b614a2cffe1f2130a09880ffd15600466
VicHfigueiredo <91691027+VicHfigueiredo@users.noreply.github.com>
2022-03-29 15:46:21 -0300

    Update constants.php

--- model/constants.php
+++ model/constants.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
+define('DB_HOST','appvictorh4.mysql.database.azure.com');
+define('DB_USER','victorfigueiredo@appvictorh4');
+define('DB_PASS','P@ssw0rd');
 define('DB_NAME','hortifrutbds');
